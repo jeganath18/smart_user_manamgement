88f908c15dbc110caf14ad5feb82a25d7580d736
Jeganath18 <jeganb007@gmail.com>
2026-05-23 22:59:12 +0530

    Add proper styling and table to the mainscreen

--- src/app/features/dashboard/dashboard-home/dashboard-home.ts
+++ src/app/features/dashboard/dashboard-home/dashboard-home.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.html',
   styleUrls: ['./dashboard-home.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
 })
 export class DashboardHome {
 
@@ -30,5 +31,77 @@ export class DashboardHome {
       img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-2.jpg"
     }
   ];
+
+  // MODAL STATES
+
+  isProfileOpen = false;
+
+  visible = false;
+
+  position = {
+    x: 0,
+    y: 0
+  };
+
+  // OPEN MODAL
+
+  openProfileModal() {
+    this.isProfileOpen = true;
+  }
+
+  // CLOSE MODAL
+
+  closeProfileModal() {
+    this.isProfileOpen = false;
+  }
+
+  // GLOW EFFECT
+
+  handleMouseMove(event: MouseEvent) {
+
+    const target = event.currentTarget as HTMLElement;
+
+    const bounds = target.getBoundingClientRect();
+
+    this.position = {
+      x: event.clientX - bounds.left,
+      y: event.clientY - bounds.top
+    };
+  }
+
+  logout() {
+
+  localStorage.clear();
+
+  window.location.href = '/login';
+
+}
+
+addMemberModal = false;
+
+memberForm = {
+  name: '',
+  email: '',
+  designation: ''
+};
+
+openAddMemberModal() {
+  this.addMemberModal = true;
+  console.log("hii");
+}
+
+closeAddMemberModal() {
+  this.addMemberModal = false;
+}
+
+createMember() {
+
+  console.log(this.memberForm);
+
+  // API CALL HERE
+
+  this.closeAddMemberModal();
+
+}
 
 }
